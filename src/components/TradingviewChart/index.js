c20620c6c885c332212ae3f21159e0aba37f968f
thanhnv2 <thanhnv2@vnext.com.vn>
2021-09-29 16:24:35 +0700

    fix bug undefined

--- src/components/TradingviewChart/index.js
+++ src/components/TradingviewChart/index.js
@@ -160,7 +160,7 @@ const TradingViewChart = ({
 
       // format numbers
       let percentChange = baseChange?.toFixed(2)
-      let formattedPercentChange = (percentChange > 0 ? '+' : '') + percentChange + '%'
+      let formattedPercentChange = percentChange ? (percentChange > 0 ? '+' : '') + percentChange + '%' : ''
       let color = percentChange >= 0 ? '#7AC51B' : '#F05359'
 
       // get the title of the chart
